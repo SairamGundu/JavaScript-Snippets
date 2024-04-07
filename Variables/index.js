49b1c7caac002303bb4d993e264e54c99252d4aa
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-07 18:23:35 +0530

    Add files via upload

---
+++ Variables/index.js
@@ -0,0 +1,16 @@
+console.log("This is a Variables code")
+
+let a = 67 // a contains 67
+// a is a identifier
+//= is a assignment operator
+//67 is a literal
+
+console.log(a);
+
+a = "Harry"
+
+console.log(a)
+
+//let 8sai = 9; //not allowed this will ba an error
+
+
