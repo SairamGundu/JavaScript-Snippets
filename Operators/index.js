048298a824f2e51c4162a16ed5e99951cfa7157a
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-08 23:53:27 +0530

    Add files via upload

---
+++ Operators/index.js
@@ -0,0 +1,103 @@
+console.log("Operators in javascript");
+
+// Arithemetic operatrs 
+
+let a = 30;
+
+let b = 20;
+
+console.log("A + B = ", a + b);
+
+console.log("A - B = ", a - b);
+
+console.log("A * B = ", a * b);
+
+console.log("A / B = ", a / b);
+
+console.log("A % B = ", a % b);
+
+console.log("A ** B = ", a ** b);
+
+console.log("++A = ", ++a);
+
+console.log("A++ = ", a++);
+
+console.log("--A = ", --a);
+
+console.log("A-- = ", a--);
+
+console.log("A = ", a);
+
+console.log("A-- = ", a--);
+
+// Assignment operatrs
+
+let c = 10;
+
+c += 5;
+
+//c=10+5=15
+console.log("c = ", c);
+
+c -= 5;
+
+//c=15-5=10
+console.log("c = ", c);
+
+c *= 5;
+
+//c=10*5=50
+console.log("c = ", c);
+
+c /= 5;
+
+//c=50/5=10
+
+console.log("c = ", c);
+
+c %= 5;
+
+//c=10%5=0
+
+console.log("c = ", c);
+
+c **= 5;
+
+//c=0**5=0
+
+console.log("c = ", c);
+
+//comparison operator
+
+let comp1 = 6;
+let comp2 = "7";
+
+console.log("comp1 == comp2 is ", comp1 == comp2);
+
+console.log("comp1!= comp2 is ", comp1 != comp2);
+
+console.log("comp1 === comp2 is ", comp1 === comp2);
+console.log("comp1 !== comp2 is ", comp1 !== comp2);
+
+console.log("comp1 > comp2 is ", comp1 > comp2);
+
+//logical Operators
+
+let x = 5;
+let y = 6;
+
+console.log(x < y && x == 5);// if botth are true then true
+
+console.log(x > y || x == 5);// if any one is true then true
+
+console.log(!false);// not of false is true
+
+console.log(!true);// not of true is false` 
+
+
+// 4 + 9 = 13
+
+// 4 is an operand
+// + is an operator
+//13 is result
+
