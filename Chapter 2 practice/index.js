8c18838f43aadcf92df98c83273aa6642c92a744
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-08 23:54:23 +0530

    Add files via upload

---
+++ Chapter 2 practice/index.js
@@ -0,0 +1,44 @@
+
+//Question 1
+
+let a = "Sairam"
+let b = "19"
+
+console.log("His name is " + a + " and his bday is on " + b)
+
+//Question 2
+console.log(typeof a)
+
+//Question 3
+const c = {
+
+    name: "Sairam",
+    age: 19,
+    gender: "Male",
+}
+
+//c = 12;
+
+//c = "12" throws an error as its a constant 
+
+//Question 4
+
+
+c['friend'] = "Ramu"
+c['name'] = "Navin"
+//change of values in a object is possible but not assignment of constant 
+console.log(c)
+
+
+//Question 5
+
+const dict = {
+
+    name: "Sairam",
+    age: 19,
+    gender: "Male",
+}
+
+console.log(dict)
+
+
